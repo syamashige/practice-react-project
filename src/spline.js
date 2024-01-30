91aa13f0d8e102ff27cb3369c6f27a98547ff9fc
Sarah <yamashige.sarah@gmail.com>
2024-01-29 20:27:39 -1000

    Add files via upload

---
+++ src/spline.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function SplineExample() {
+  return (
+    <Spline scene="https://prod.spline.design/06LhQQCRCeJ36U39/scene.splinecode" />
+  );
+}
---
+++ src/test.js
@@ -0,0 +1,10 @@
+
+function Test () {
+    return (
+        <div id="test" style={{height: "50vh", backgroundColor: "pink", padding: 20, border: "2px solid purple"}}>
+           <h2 style={{border: "2px solid blue", marginTop: 0}}>Hello World</h2>
+        </div>
+    )
+}
+
+export default Test;
