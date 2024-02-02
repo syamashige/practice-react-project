b0c47308223fc39c7453ed66e02c3a4e4ca7fb44
Sarah Yamashige <sarahyamashige@Sarahs-MacBook-Pro.local>
2024-02-01 14:43:16 -1000

    Cleaning up the code that was commented out.

--- src/sections/1_welcome_section/welcome.js
+++ src/sections/1_welcome_section/welcome.js
@@ -1,15 +1,37 @@
 import '../../global/global.css';
-// import SplineMiniRoomExample from '../../components/spline_scenes/spline_mini_room_example.js';
+import './welcome.css';
+import SplineMiniRoomExample from '../../components/spline_scenes/spline_mini_room_example.js';
+
+// export default function Welcome () {
+//     return (
+//         <div id="welcome" className='SectionStyles'>
+//             <span className='SectionTitle'>Welcome</span>
+//             <div className='AltSectionGrid'>
+//                 <div className='SectionColumnGridAlt'>
+//                     <div className='NameTitle'>
+//                         <h1 className='NameHeader'>Sarah Yamashige</h1>
+//                     </div>
+//                 </div>
+//                 <div className='SectionColumnGridTwo'>
+//                     <SplineMiniRoomExample/>
+//                 </div>
+//             </div>
+//         </div>
+//     )
+// }
 
 export default function Welcome () {
     return (
         <div id="welcome" className='SectionStyles'>
             <span className='SectionTitle'>Welcome</span>
-            <div className='AltSectionGrid'>
-                <div className='SectionColumnGridAlt'>
-                    <h1>Welcome</h1>
+            <div className='WelcomeContainer'>
+                <div className='NameHeaderContainer'>
+                    <div className='NameTitle'>
+                        <h1 className='NameHeader'>Sarah Yamashige</h1>
+                    </div>
                 </div>
-                <div className='SectionColumnGridTwo'>
+                <div className='HeroImageContainer'>
+                    <SplineMiniRoomExample/>
                 </div>
             </div>
         </div>
--- src/sections/5_contact_section/contact.js
+++ src/sections/5_contact_section/contact.js
@@ -1,28 +1,29 @@
 import '../../global/global.css';
+import './contact.css';
+import SplineMiniRoomExample from '../../components/spline_scenes/spline_mini_room_example';
 
 export default function Contact () {
     return (
         <div id="contact-me" className='SectionStyles'>
             <span className='SectionTitle'>Contact</span>
-            {/* <h1>Contact Me</h1> */}
-            <div className='AltSectionGrid'>
-                <div className='SectionColumnGridAlt'>
-                    <h2 >Let's Chat!</h2>
+            <div className='ContactContainer'>
+                <div className='ContactInfo'>
+                    <h2>Let's Chat!</h2>  
                     <div className='GroupedInfo'>
                        <h3>Email Me</h3>
                        <a className="email" href="yamashige.sarah@gmail.com">yamashige.sarah@gmail.com</a>
                     </div>
                     <div className='GroupedInfo'>
                        <h3>Connect With Me</h3>
-                       <a className="email" href="yamashige.sarah@gmail.com">yamashige.sarah@gmail.com</a>
+                       <a className="linkedin" href="www.linkedin.com/in/sarah-yamashige">linkedin.com/in/sarah-yamashige</a>
                     </div>
                     <div className='GroupedInfo'>
                        <h3>Call Me</h3>
-                       <a className="email" href="yamashige.sarah@gmail.com">yamashige.sarah@gmail.com</a>
+                       <span className='phone'>(808) 214-0731</span>
                     </div>
                 </div>
-                <div className='SectionColumnGridTwo'>
-                    
+                <div className='ContactImageContainer'>
+                    <SplineMiniRoomExample/>
                 </div>
             </div>
         </div>
